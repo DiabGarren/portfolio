a3820abd8596b9a2395edb8c69e2e40973d5ea97
DiabGarren <garrendiab@gmail.com>
2023-01-12 17:12:47 +0200

    More text in terminal

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
-let welcome = "hello there"
-let hintText = "try clicking one of the letters";
-let clear = "cls"
+let greeting = "hello there";
+let welcome = "welcome to my project portfolio"
+let hintText = "click a letter to view a project";
+let clear = "cls";
 
 const terminal = document.querySelector("#terminal");
 if (terminal == null) {} else {
@@ -12,20 +13,29 @@ if (terminal == null) {} else {
 
     async function typeTerminal() {
         while (true){
-            await wait(1000);
-            for (let i = 0; i < welcome.length; i++) {
-                terminal.innerHTML += welcome[i];
+            await wait(2000);
+            for (let i = 0; i < greeting.length; i++) {
+                terminal.innerHTML += greeting[i];
     
-                await wait(Math.floor(Math.random() * (500 - 200 + 1) + 200));
+                await wait(Math.floor(Math.random() * (275 - 75 + 1) + 75));
             }
     
             terminal.innerHTML += "<br>garren-diab>";
             await wait(1500);
+
+            for (let i = 0; i < welcome.length; i++) {
+                terminal.innerHTML += welcome[i];
+    
+                await wait(Math.floor(Math.random() * (275 - 75 + 1) + 75));
+            }
+    
+            terminal.innerHTML += "<br>garren-diab>";
+            await wait(3000);
     
             for (let i = 0; i < hintText.length; i++) {
                 terminal.innerHTML += hintText[i];
     
-                await wait(Math.floor(Math.random() * (500 - 200 + 1) + 200));
+                await wait(Math.floor(Math.random() * (275 - 75 + 1) + 75));
             }
     
             terminal.innerHTML += "<br>garren-diab>";
@@ -34,10 +44,10 @@ if (terminal == null) {} else {
             for (let i = 0; i < clear.length; i++) {
                 terminal.innerHTML += clear[i];
     
-                await wait(Math.floor(Math.random() * (500 - 200 + 1) + 200));
+                await wait(Math.floor(Math.random() * (275 - 75 + 1) + 75));
             }
     
-            await wait(500);
+            await wait(3000);
             terminal.innerHTML = "garren-diab>";
         }
     };
