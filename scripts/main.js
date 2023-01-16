c3e1db308dc9f19c9716fbefaf1c79e9c38e0cb6
DiabGarren <garrendiab@gmail.com>
2023-01-16 18:09:19 +0200

    Staggered letter load

--- scripts/main.js
+++ scripts/main.js
@@ -1,53 +1,55 @@
 const messages = ["hello there", "this is my project portfolio", "click a letter to begin"];
-
-console.log(window.innerWidth);
-
 const terminal = document.querySelector("#terminal");
+
 const wait = (milliseconds) => {
     return new Promise(resolve => {
         setTimeout(resolve, milliseconds);
     });
 };
 
-if (terminal == null) {} else {
-    async function typeTerminal() {
-        while (true) {
-            await wait(2000);
+async function typeTerminal() {
+    while (true) {
+        await wait(2000);
 
-            for (let j = 0; j < messages.length; j++) {
+        for (let j = 0; j < messages.length; j++) {
 
-                for (let i = 0; i < messages[j].length; i++) {
-                    terminal.innerHTML += messages[j][i];
+            for (let i = 0; i < messages[j].length; i++) {
+                terminal.innerHTML += messages[j][i];
 
-                    await wait(Math.floor(Math.random() * (275 - 75 + 1) + 75));
-                }
-                await wait(1500);
+                await wait(Math.floor(Math.random() * (275 - 75 + 1) + 75));
+            }
+            await wait(1500);
 
-                for (let i = terminal.innerHTML.length; i > 13; i--) {
-                    terminal.innerHTML = terminal.innerHTML.substring(0, i);
+            for (let i = terminal.innerHTML.length; i > 13; i--) {
+                terminal.innerHTML = terminal.innerHTML.substring(0, i);
 
-                    await wait(Math.floor(Math.random() * (100 - 45 + 1) + 45));
-                }
+                await wait(Math.floor(Math.random() * (100 - 45 + 1) + 45));
             }
         }
-    };
-    typeTerminal();
-}
+    }
+};
+typeTerminal();
 
 let letters = ["#m-letter", "#y-letter", "#p-letter", "#r-letter", "#o-letter", "#j-letter", "#e-letter", "#c-letter", "#t-letter", "#s-letter"];
 let positions = ["topleft", "topright", "botleft", "botright"];
 
 async function loadLetters(letterContainer) {
-    let length = letterContainer.length-1;
+    let length = letterContainer.length - 1;
+
     for (let i = 0; i < length; i += 2) {
-        await wait(Math.floor(Math.random() * (350 - 200 + 1) + 200));
-        for (let j = 0; j < positions.length; j++){
+        await wait(Math.floor(Math.random() * (300 - 150 + 1) + 150));
+        for (let j = 0; j < positions.length; j++) {
             document.querySelector(letterContainer[i]).children[0].children[j].setAttribute("src", `images/${letterContainer[i][1]}-${positions[j]}.webp`);
-            document.querySelector(letterContainer[length-i]).children[0].children[3-j].setAttribute("src", `images/${letterContainer[length-i][1]}-${positions[3-j]}.webp`);
-            await wait(Math.floor(Math.random() * (150 - 75 + 1) + 75));
+            await wait(Math.floor(Math.random() * (75 - 50 + 1) + 50));
+            document.querySelector(letterContainer[length - i]).children[0].children[3 - j].setAttribute("src", `images/${letterContainer[length - i][1]}-${positions[3-j]}.webp`);
+            await wait(Math.floor(Math.random() * (100 - 50 + 1) + 50));
         }
     }
-}
+};
+
+async function reloadLetters(letterContainer) {
+
+};
 
 loadLetters(letters);
 
