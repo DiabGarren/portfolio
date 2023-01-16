331605810f98da4f3bced81badffd079e9e033a8
DiabGarren <garrendiab@gmail.com>
2023-01-16 17:54:02 +0200

    Letters load in

--- scripts/main.js
+++ scripts/main.js
@@ -32,10 +32,24 @@ if (terminal == null) {} else {
         }
     };
     typeTerminal();
-
 }
 
 let letters = ["#m-letter", "#y-letter", "#p-letter", "#r-letter", "#o-letter", "#j-letter", "#e-letter", "#c-letter", "#t-letter", "#s-letter"];
+let positions = ["topleft", "topright", "botleft", "botright"];
+
+async function loadLetters(letterContainer) {
+    let length = letterContainer.length-1;
+    for (let i = 0; i < length; i += 2) {
+        await wait(Math.floor(Math.random() * (350 - 200 + 1) + 200));
+        for (let j = 0; j < positions.length; j++){
+            document.querySelector(letterContainer[i]).children[0].children[j].setAttribute("src", `images/${letterContainer[i][1]}-${positions[j]}.webp`);
+            document.querySelector(letterContainer[length-i]).children[0].children[3-j].setAttribute("src", `images/${letterContainer[length-i][1]}-${positions[3-j]}.webp`);
+            await wait(Math.floor(Math.random() * (150 - 75 + 1) + 75));
+        }
+    }
+}
+
+loadLetters(letters);
 
 async function explode(e) {
     for (let i = 0; i < letters.length; i++) {
